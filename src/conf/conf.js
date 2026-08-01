ff2ae9408757bbb31ee04940335a17d36500bc2d
newwmohan-jpg <newwmohan@gmail.com>
2026-08-02 02:05:54 +0530

    setup env file adding to conf file for string access and no type mismatch

---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+
+    appwriteurl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_DATABASE_ID),
+    appwriteTableId : String(import.meta.env.VITE_APPWRITE_TABLES_ID),
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+
+export default conf 
